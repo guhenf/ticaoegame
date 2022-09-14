65eb2b26454fc078620703eafc8c9437e74e677c
Gustavo Franco <guhenf@gmail.com>
2022-09-14 11:37:09 -0300

    feat: rounds conditions wins/draws

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 const cells = document.querySelectorAll('.cell')
 const board = document.querySelector('.board')
+const containerMessage = document.querySelector('.containerMessage')
+const roundMessage = document.querySelector('.roundMessage')
+const roundRestart = document.querySelector('.roundRestart')
 
 let currentTurn
 
+const winnerCombinations = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[2, 4, 6],
+	[0, 4, 8],
+]
+
 const start = () => {
+	currentTurn = false
+
 	for (const cell of cells) {
+		cell.classList.remove('o')
+		cell.classList.remove('x')
+		cell.removeEventListener('click', handleClick)
 		cell.addEventListener('click', handleClick, { once: true })
 	}
 
-	currentTurn = false
-
-	board.classList.add('x')
+	setFirstPlayer()
+	containerMessage.classList.remove('display')
 }
 
-const changePlayer = () => {
-	currentTurn = !currentTurn
+const winnerCondition = (currentPlay) => {
+	return winnerCombinations.some((combination) => {
+		return combination.every((index) => {
+			return cells[index].classList.contains(currentPlay)
+		})
+	})
+}
 
+const drawCondition = () => {
+	return [...cells].every((cell) => {
+		return cell.classList.contains('x') || cell.classList.contains('o')
+	})
+}
+
+const endGame = (draw) => {
+	if (draw) {
+		roundMessage.innerHTML = 'Draw!'
+	} else {
+		roundMessage.innerHTML = currentTurn ? 'O Win!' : 'X Win!'
+	}
+	containerMessage.classList.add('display')
+}
+
+roundRestart.addEventListener('click', start)
+
+const setFirstPlayer = () => {
 	board.classList.remove('x')
 	board.classList.remove('o')
 
@@ -24,6 +65,11 @@ const changePlayer = () => {
 	} else {
 		board.classList.add('x')
 	}
+}
+
+const changePlayer = () => {
+	currentTurn = !currentTurn
+	setFirstPlayer()
 }
 
 const cellMark = (cell, playToAdd) => {
@@ -35,7 +81,17 @@ const handleClick = (e) => {
 	const playToAdd = currentTurn ? 'o' : 'x'
 
 	cellMark(cell, playToAdd)
-	changePlayer()
+
+	const win = winnerCondition(playToAdd)
+	const draw = drawCondition()
+
+	if (win) {
+		endGame(false)
+	} else if (draw) {
+		endGame(true)
+	} else {
+		changePlayer()
+	}
 }
 
 start()
