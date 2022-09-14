b2ae0756ed08170e17571c8dc795864b645cf726
Gustavo Franco <guhenf@gmail.com>
2022-09-14 12:58:38 -0300

    feat: scoreboard and reset scoreboard

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ const board = document.querySelector('.board')
 const containerMessage = document.querySelector('.containerMessage')
 const roundMessage = document.querySelector('.roundMessage')
 const roundRestart = document.querySelector('.roundRestart')
+const winningsCountP1 = document.querySelector('.winningsCountP1')
+const winningsCountP2 = document.querySelector('.winningsCountP2')
+const restartScoreboard = document.querySelector('.restartScoreboard')
 
 let currentTurn
+
+let winsP1 = 0
+let winsP2 = 0
 
 const winnerCombinations = [
 	[0, 1, 2],
@@ -50,6 +56,15 @@ const endGame = (draw) => {
 		roundMessage.innerHTML = 'Draw!'
 	} else {
 		roundMessage.innerHTML = currentTurn ? 'O Win!' : 'X Win!'
+		if (!currentTurn) {
+			console.log(currentTurn, winsP1)
+			winsP1++
+			winningsCountP1.innerHTML = winsP1
+		} else {
+			console.log(currentTurn, winsP2)
+			winsP2++
+			winningsCountP2.innerHTML = winsP2
+		}
 	}
 	containerMessage.classList.add('display')
 }
@@ -93,5 +108,15 @@ const handleClick = (e) => {
 		changePlayer()
 	}
 }
+
+const winnerCounter = () => {
+	winsP1 = 0
+	winningsCountP1.innerHTML = winsP1
+
+	winsP2 = 0
+	winningsCountP2.innerHTML = winsP2
+}
+
+restartScoreboard.addEventListener('click', winnerCounter)
 
 start()
